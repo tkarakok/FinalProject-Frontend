319983982ac909ce1be7bc716d75783115f410ff
tkarakok <karakoktunahan78@gmail.com>
2023-11-12 23:25:16 +0300

    response models imported

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
-export class ProductComponent {
-  product1: any = { productId: 1, productName: 'Phone', categoryId: 1, unitPrice: 5 };
-  product2: any = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 5 };
-  product3: any = { productId: 3, productName: 'Mouse', categoryId: 1, unitPrice: 5 };
-  product4: any = { productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 5 };
-  product5: any = { productId: 5, productName: 'Camera', categoryId: 1, unitPrice: 5 };
-
-  products = [this.product1, this.product2, this.product3, this.product4, this.product5]
+export class ProductComponent 
+{
+  products : Product[] = [];
 }
