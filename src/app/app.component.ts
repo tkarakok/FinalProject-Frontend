e1a1d9b0b7059ac2003404a751fb47e7724b8ca0
tkarakok <karakoktunahan78@gmail.com>
2023-11-13 00:09:59 +0300

    api connection and set datas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,20 @@ import { RouterOutlet } from '@angular/router';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { ProductComponent } from './components/product/product.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,CategoryComponent,NaviComponent,ProductComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    CategoryComponent,
+    NaviComponent,
+    ProductComponent,
+    HttpClientModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from '../../models/productResponseModel';
+
 
 @Component({
   selector: 'app-product',
@@ -9,7 +12,21 @@ import { Product } from '../../models/product';
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
-export class ProductComponent 
+export class ProductComponent implements OnInit 
 {
   products : Product[] = [];
+  apiUrl = "https://localhost:44308/api/products/getall"
+  constructor (private httpClient : HttpClient) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.httpClient
+    .get<ProductResponseModel>(this.apiUrl)
+    .subscribe(response => {
+      this.products = response.data
+    });
+  }
 }
